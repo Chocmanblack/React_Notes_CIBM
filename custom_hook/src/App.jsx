c516ad922e7b3d206a02174dffddfbbb088c627f
chocmanblack <leonardojacum@gmail.com>
2025-04-22 20:57:12 -0400

    Convertido custom_hook en carpeta normal, sin submódulo

---
+++ custom_hook/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import FetchData from './Components/FetchData'
+import FetchYogaData from './Components/FetchYogaData'
+
+function App() {
+  return (
+ <>
+ 
+ {/* <FetchData/> */}
+ <FetchYogaData/>
+ </>
+
+
+  )
+}
+
+export default App
